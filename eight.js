b8d84f33df730fc21396d18fce96bb850333400a
elinabhasin <elinabhasin27@gmail.com>
2024-04-02 23:24:36 +0530

    Added response text file

--- eight.js
+++ eight.js
@@ -1,1 +1,8 @@
 alert("We're watching you!! HAHA :)");
+
+fetch('response.txt')
+.then(response=>response.text())
+.then(data=>{
+    document.getElementById('response').innerText(data);
+})
+.catch(error=>console.error('Error fetching response: ',error));
