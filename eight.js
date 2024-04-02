8cf367813a60381ae9ed513796acad1e11eb8561
elinabhasin <elinabhasin27@gmail.com>
2024-04-03 00:26:17 +0530

    Added the question input button

--- eight.js
+++ eight.js
@@ -3,6 +3,6 @@ alert("We're watching you!! HAHA :)");
 fetch('response.txt')
 .then(response=>response.text())
 .then(data=>{
-    document.getElementById('response').innerText(data);
+    document.getElementById('response').innerText=data;
 })
 .catch(error=>console.error('Error fetching response: ',error));
