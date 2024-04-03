9c8db4f7f845f90e7fe9eab453345e2478ad309f
elinabhasin <elinabhasin27@gmail.com>
2024-04-03 23:20:56 +0530

    Added in varying repsonses according to user input

--- eight.js
+++ eight.js
@@ -1,8 +1,17 @@
 alert("We're watching you!! HAHA :)");
 
-fetch('response.txt')
-.then(response=>response.text())
-.then(data=>{
-    document.getElementById('response').innerText=data;
-})
-.catch(error=>console.error('Error fetching response: ',error));
+let responses=["That sandwhich was...delicious","Yes","Maybe","No","You are the father","New Ball. Who this?","Can't you figure that our yourself?","Please shake again","Get a life, loser.","HA HA HA HA HAAAA","Trust me, you don't want to know.","It's hard to say.","I have a headache. Ask again later.","Meh.","Shatap","OBVIOUSLY","Like i care.",":)","Ask Dad."]
+
+
+
+
+
+function getRes(){
+    let index=Math.floor(Math.random() * (responses.length));
+
+    let choice=responses[index];
+    document.getElementById('response').innerText=choice;
+}
+
+
+
